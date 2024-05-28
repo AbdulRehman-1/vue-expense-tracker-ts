87923567fd942e68a3955d76875b5dbb515ad1f6
AbdulRehman-1 <abdul.rehman.wb12@gmail.com>
2024-05-29 02:15:44 +0500

    completed ui for IncomeExpense component

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,12 @@
   <AppHeader />
   <div class="container">
     <AppBalance />
+    <IncomeExpense />
   </div>
 </template>
 
 <script setup>
 import AppHeader from './components/AppHeader.vue'
 import AppBalance from './components/AppBalance.vue'
+import IncomeExpense from './components/IncomeExpense.vue'
 </script>

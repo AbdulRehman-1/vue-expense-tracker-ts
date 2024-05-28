e60c7af843e7950d4a40b7664bb09969d4bb5f30
AbdulRehman-1 <abdul.rehman.wb12@gmail.com>
2024-05-29 02:13:53 +0500

    completed ui for balance component

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,11 @@
-<template><AppHeader /></template>
+<template>
+  <AppHeader />
+  <div class="container">
+    <AppBalance />
+  </div>
+</template>
 
 <script setup>
 import AppHeader from './components/AppHeader.vue'
+import AppBalance from './components/AppBalance.vue'
 </script>
